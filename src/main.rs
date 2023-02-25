4a7feee33612ad925a878494da7f5968c1eeea72
qb <kalyan.raparthi@hotmail.com>
2023-02-26 01:53:32 +0530

    ip change

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::net::{TcpListener, TcpStream};
 
 fn main() -> io::Result<()> {
     let mut buffer = [0; 17];
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let listener = TcpListener::bind("13.228.225.19:80")?;
 
     for i in listener.incoming() {
         match i {
